3b9570959844a0420249827fce41d8d95e9c62de
OlegMilkin <olegek.milkin@gmail.com>
2021-11-24 15:41:41 +0200

    react deploy to github pages

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'}>
-          <Redirect to={process.env.PUBLIC_URL + '/employees'} />
+        <Route exact path='/yalantis'>
+          <Redirect to='/yalantis/employees' />
         </Route>
-        <Route path={process.env.PUBLIC_URL + '/employees'} component={Employees} />
+        <Route path='/yalantis/employees' component={Employees} />
+        <Route path='*'>
+          <div>404 Not Found</div>
+        </Route>
       </Switch>
     </Router>
   )
