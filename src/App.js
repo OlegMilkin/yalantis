75f11ba1af55065692e6f595bd190b5a6b4557da
OlegMilkin <olegek.milkin@gmail.com>
2021-11-24 16:36:41 +0200

    react deploy to github pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Employees from './components/Employees'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/yalantis/employees' component={Employees} />
-        <Route path='*'>
-          <div>404 Not Found</div>
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path='/'>
+        <Redirect to='/employees' />
+      </Route>
+      <Route path='/employees' component={Employees} />
+      <Route path='*'>
+        <div>404 Not Found</div>
+      </Route>
+    </Switch>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App'
 import store from './redux/store'
 import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>  
   </Provider>,
   document.getElementById('root')
 )
